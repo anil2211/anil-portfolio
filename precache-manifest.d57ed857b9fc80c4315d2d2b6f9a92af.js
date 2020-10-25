e5480d023a6bd157518afe80671e4d038cccf820
anil2211 <anilc2812@gmail.com>
2020-10-25 22:11:16 +0530

    Updates

---
+++ precache-manifest.d57ed857b9fc80c4315d2d2b6f9a92af.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b73243f96215d6364dfbe6a1ffe155f4",
+    "url": "/anil-portfolio/index.html"
+  },
+  {
+    "revision": "ca9ff8aa434774031cf7",
+    "url": "/anil-portfolio/static/css/3.879e9400.chunk.css"
+  },
+  {
+    "revision": "dc46b37c0088163370f7",
+    "url": "/anil-portfolio/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "9ffe47528443e2408903",
+    "url": "/anil-portfolio/static/css/main.6e6569fc.chunk.css"
+  },
+  {
+    "revision": "e5f2555b8ebf764c7d34",
+    "url": "/anil-portfolio/static/js/2.0af089cf.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/anil-portfolio/static/js/2.0af089cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca9ff8aa434774031cf7",
+    "url": "/anil-portfolio/static/js/3.71878dce.chunk.js"
+  },
+  {
+    "revision": "dc46b37c0088163370f7",
+    "url": "/anil-portfolio/static/js/4.d56b3f80.chunk.js"
+  },
+  {
+    "revision": "9ffe47528443e2408903",
+    "url": "/anil-portfolio/static/js/main.80b81aa8.chunk.js"
+  },
+  {
+    "revision": "828d56e5886154efdfad",
+    "url": "/anil-portfolio/static/js/runtime-main.d2b65ffc.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/anil-portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/anil-portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/anil-portfolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "94e2cc28f9eefc37bccdafd38a91740c",
+    "url": "/anil-portfolio/static/media/data science1.94e2cc28.jpg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/anil-portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "9b6d7217c8e9f5953f207c33b0c77645",
+    "url": "/anil-portfolio/static/media/django logo.9b6d7217.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/anil-portfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "6fce783f47076417a364e4ef7f51839f",
+    "url": "/anil-portfolio/static/media/machine learning.6fce783f.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/anil-portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "fdfd8925f8720001ca1662af0b879526",
+    "url": "/anil-portfolio/static/media/mongodblogi.fdfd8925.png"
+  },
+  {
+    "revision": "91dfb043d04f90b58ba5b395f4f925ec",
+    "url": "/anil-portfolio/static/media/relinns_logo.91dfb043.jpg"
+  },
+  {
+    "revision": "613387ca1e28e0f2e9f1fbcfb878bc7f",
+    "url": "/anil-portfolio/static/media/sebiz_logo.613387ca.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/anil-portfolio/static/media/skill.34409398.svg"
+  }
+]);
